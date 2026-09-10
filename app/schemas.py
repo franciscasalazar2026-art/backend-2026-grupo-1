7c30ff5bd7064272c4b66cbdb4466cb9f9a7d103
camilqqq <camila.fuentes2026@alu.uct.cl>
2026-09-10 16:38:46 -0300

    Crea esquema base para Cita

--- app/schemas.py
+++ app/schemas.py
@@ -17,3 +17,17 @@ class ClienteUpdate(BaseModel):
     nombre: Optional[str] = None
     email: Optional[EmailStr] = None
     telefono: Optional[str] = None
+
+class Cliente(ClienteBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+# --- ESQUEMAS DE CITA ---
+
+class CitaBase(BaseModel):
+    cliente_id: int
+    fecha_hora: datetime
+    motivo: str
+    estado: EstadoCita = EstadoCita.PENDIENTE
